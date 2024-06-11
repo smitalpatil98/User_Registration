f22620eb7912e69fe5c3adf4b93ab27af180cc6b
smitalpatil98 <patilsmital.1101998gmail.com>
2024-06-11 16:00:24 +0530

    UC3_MobileFormat

--- src/main/java/com/user/UserRegistration.java
+++ src/main/java/com/user/UserRegistration.java
@@ -6,20 +6,20 @@ import java.util.regex.Matcher;
 
 public class UserRegistration
 {
-    public void validateName()
+    public void validateNumber()
     {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter Email Id");
-    String emailid = sc.nextLine();
-    String regex=  "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    System.out.println("Enter Mobile Number");
+    String MobileNumber = sc.nextLine();
+    String regex = "^[0-9]{2} [0-9]{10}$";
     Pattern p = Pattern.compile(regex);
-    Matcher m = p.matcher(emailid);
+    Matcher m = p.matcher(MobileNumber);
     boolean b = m.matches();
     System.out.println(b);
     }
     public static void main(String[] args)
     {
         UserRegistration ur = new UserRegistration();
-        ur.validateName();
+        ur.validateNumber();
     }
 }
