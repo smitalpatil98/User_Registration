797fdc21fbbd35b9ace038365b62a74cec8646a0
smitalpatil98 <patilsmital.1101998gmail.com>
2024-06-11 14:36:21 +0530

    file added

---
+++ src/main/java/com/user/UserRegistration.java
@@ -0,0 +1,25 @@
+package com.user;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class UserRegistration
+{
+    public void validateName()
+    {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter first name");
+    String firstName = sc.nextLine();
+    String regex = "[A-Z][a-z]{4,10}";
+    Pattern p = Pattern.compile(regex);
+    Matcher m=p.matcher(firstName);
+    boolean b = m.matches();
+    System.out.println(b);
+    }
+    public static void main(String[] args)
+    {
+        UserRegistration ur = new UserRegistration();
+        ur.validateName();
+    }
+}
